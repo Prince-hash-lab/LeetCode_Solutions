a15a8c46b87e2a09fc7dbfebbf7a3f3d02b9a8dd
Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-06-20 09:57:32 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (38.18%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int flag=0;
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            if(nums[i]>nums[i+1]) flag++;
+            if (flag==2) return false ;
+        }
+        if(flag !=0 and nums[0]<nums[nums.size()-1]) return false;
+        return true ;
+    }
+};
