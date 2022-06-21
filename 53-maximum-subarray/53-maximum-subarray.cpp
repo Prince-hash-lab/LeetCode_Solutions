86d5a158b0550ba78ac5d3f90f0522dcc2b80c06
Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-06-21 11:44:22 +0530

    Time: 150 ms (56.05%), Space: 67.9 MB (11.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int sz=nums.size();
-        for(int i=1; i<sz; i++)
-        {
-            nums[i]+=nums[i-1];
-        }
         int maxval=nums[0];
-        int mini=nums[0];
+        int res=nums[0];
         for(int i=1; i<sz; i++)
         {
-            maxval=max(nums[i], max(maxval, nums[i]-mini));
-            mini=min(mini, nums[i]);
+            maxval=max(nums[i], maxval+nums[i]);
+            res=max(maxval, res);
         }
-        return maxval;
+        
+     
+        return res;
             
     }
 };
