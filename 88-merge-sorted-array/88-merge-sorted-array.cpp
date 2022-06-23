d7d7456a8e2e89574163a7a4a33109fefcd1d9e1
Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-06-23 18:07:59 +0530

    Time: 3 ms (74.37%), Space: 9.1 MB (71.58%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int ind1=0, ind2=0, ind=0;
-        vector<int> temp(m+n);
-        while(ind1<m and ind2<n)
+        if(n==0) return ;
+        for(int i=0; i<m; i++)
+       {
+           if(nums1[i]>nums2[0])
+           {
+               swap(nums1[i], nums2[0]);
+               for(int j=1; j<n; j++)
+               {
+                   if(nums2[j]<nums2[j-1]) swap(nums2[j], nums2[j-1]);
+               }
+           }
+       }
+        for(int i=0; i<n; i++)
         {
-            if(nums1[ind1]<nums2[ind2]) temp[ind++]=nums1[ind1++];
-            else temp[ind++]=nums2[ind2++];
+            nums1[m+i]=nums2[i];
         }
-        while(ind1<m) temp[ind++]=nums1[ind1++];
-        while(ind2<n) temp[ind++]=nums2[ind2++];
-        ind--;
-        // for(int i=0 ;i<ind; i++) nums1[i]=temp[i];
-        while(ind>=0)
-        {
-            nums1[ind--]=temp[ind];
-        }
-        // return nums1;
     }
 };
