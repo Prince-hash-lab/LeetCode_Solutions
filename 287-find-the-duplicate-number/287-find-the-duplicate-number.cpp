02827cfc672899042036ea68b7c43ebd244b4420
Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-06-23 12:23:11 +0530

    Time: 183 ms (47.97%), Space: 61.2 MB (91.02%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int sz=nums.size(), sum=0;
-       sort(nums.begin(), nums.end());
-        for(int i=1; i<sz; i++)
+        int sz=nums.size(), val=-1;
+      
+        for(int i=0; i<sz; i++)
         {
-            if(nums[i]==nums[i-1]) return nums[i];
+            if(nums[abs(nums[i])-1]<0) return abs(nums[i]);
+            nums[abs(nums[i])-1]*=-1;
         }
         return -1;
     }
