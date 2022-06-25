79be4c0b86e14f7fa04a2801403ade881cf44c0f
Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-06-25 09:43:56 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (29.08%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         if(n==0) return ;
-        for(int i=0; i<m; i++)
-       {
-           if(nums1[i]>nums2[0])
-           {
-               swap(nums1[i], nums2[0]);
-               for(int j=1; j<n; j++)
-               {
-                   if(nums2[j]<nums2[j-1]) swap(nums2[j], nums2[j-1]);
-               }
-           }
-       }
+        int gap=(m+n)/2;
         for(int i=0; i<n; i++)
         {
             nums1[m+i]=nums2[i];
         }
+        while(gap!=0)
+        {
+            for(int i=0; (i+gap)<(m+n); i++)
+            {
+                if(nums1[i]>nums1[i+gap])
+                    swap(nums1[i], nums1[i+gap]);
+            }
+            if(gap==1) gap=0;
+            else
+                gap=ceil(floor(gap)/2);
+        }
+        
     }
 };
