91e061c60de58c96bb62ec30aade3dda3a12e1c6
Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-06-25 13:04:05 +0530

    Time: 3 ms (89.92%), Space: 10.7 MB (31.83%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -12,31 +12,23 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* curr1=head, *curr2=head ;
-        if(head==NULL) return NULL;
+        if(head==NULL) 
+            return NULL;
         while(n--!=0)
-        {
-            curr1=curr1->next;
-        }
-        if(curr1==NULL) 
-        {
-            head=curr2->next;
-            delete(curr2);
-            return head;
-        }
-        while(curr1!=NULL and curr1->next!=NULL)
+             curr1=curr1->next;
+    
+        if(!curr1) 
+            return curr2->next;
+        
+        while(curr1->next!=NULL)
         {
             curr1=curr1->next;
             curr2=curr2->next;
         }
-        if(curr2->next==NULL) 
-        {   delete(curr2);
-            return NULL;
-        }
-       
         
-        curr1=curr2->next;
+        if(curr2->next==NULL) 
+            return NULL;
         curr2->next=curr2->next->next;
-        delete(curr1);
         return head;
     }
 };
