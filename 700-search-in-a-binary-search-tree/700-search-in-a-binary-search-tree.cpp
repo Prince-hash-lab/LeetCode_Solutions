6d295a5a4a88d16f8ed02ce14a8ba4e799e19d3b
RajRishi Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-07-04 12:39:08 +0530

    Time: 87 ms (9.33%), Space: 34.7 MB (66.05%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL) return NULL;
+        if(root->val==val) return root;
+        TreeNode* l1=searchBST(root->left, val);
+        TreeNode* r1=searchBST(root->right, val);
+        if(l1!=NULL) return l1;
+        else if(r1!=NULL) return r1;
+        else return NULL;
+        
+    }
+};
