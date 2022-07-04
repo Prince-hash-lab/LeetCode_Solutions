e6bee83c2b6b4a8930b7fef090fee2e007d72b3e
RajRishi Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-07-04 12:16:19 +0530

    Time: 19 ms (28.35%), Space: 17.4 MB (51.67%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   // if(root==NULL) return 0;
+   //      queue<TreeNode*> qu;
+   //      qu.push(root);
+   //      int res=1, val=0;
+   //      while(!qu.empty())
+   //      {
+   //          TreeNode* temp=
+   //      }
+    int widthOfBinaryTree(TreeNode* root) {
+       if(root==NULL) return 0;
+        queue<pair<TreeNode*, long long>> qu1;
+        long long  res=1, currid, lmost, rmost, currmin;
+        qu1.push({root, 0});
+        while(!qu1.empty())
+        {
+            long long  sz=qu1.size();
+            currmin=qu1.front().second;
+            for(long long i=0; i<sz; i++)
+            {
+                TreeNode* temp=qu1.front().first;
+                long long val=qu1.front().second;
+                currid=val-currmin;
+                if(i==0) lmost=currid;
+                if(i==sz-1) rmost=currid;
+                if(temp->left) qu1.push({temp->left, (currid * 2) +1 });
+                if(temp->right) qu1.push({temp->right, (currid * 2) +2 });
+                qu1.pop();
+            }
+            res=max(res, rmost-lmost+1);
+        }
+        return res;
+        
+    }
+};
