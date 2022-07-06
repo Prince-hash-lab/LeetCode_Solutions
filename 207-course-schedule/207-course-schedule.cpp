612bc04c4729c24412836c663fed5e72c1cd9db2
RajRishi Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-07-06 15:17:32 +0530

    Time: 43 ms (28.67%), Space: 13 MB (97.85%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
-    bool dfs( vector<int> adj[], vector<bool> &vis, vector<bool> &recst,int sr)
-    {
-        vis[sr]=true;
-        recst[sr]=true;
-        // if(adj[sr].size()>0)
-        for(auto x : adj[sr])
-        {
-            if(!vis[x] and dfs(adj, vis,recst,  x)) return true;
-            else if(recst[x]) return true;
-        }
-        recst[sr]=false;
-        return false;
-    }
-    bool canFinish(int v, vector<vector<int>>& edges) {
-       vector<int> adj[v];
+    // bool dfs( vector<int> adj[], vector<bool> &vis, vector<bool> &recst,int sr)
+    // {
+    //     vis[sr]=true;
+    //     recst[sr]=true;
+    //     // if(adj[sr].size()>0)
+    //     for(auto x : adj[sr])
+    //     {
+    //         if(!vis[x] and dfs(adj, vis,recst,  x)) return true;
+    //         else if(recst[x]) return true;
+    //     }
+    //     recst[sr]=false;
+    //     return false;
+    // }
+    bool canFinish(int V, vector<vector<int>>& edges) {
+       vector<int> adj[V];
        int nedge=edges.size();
         for(int i=0; i<nedge; i++)
         {
             adj[edges[i][0]].push_back(edges[i][1]);
         }
-        vector<bool> vis(v, false);
-        vector<bool> recst(v, false);
-        for(int i=0; i<v; i++)
-        {
-            if(!vis[i] and dfs(adj, vis,recst,  i)) return false;
-        }
+         vector<int> indeg(V, 0);
+        int count=0;
+          for(int i=0; i<V; i++)
+	    {
+	        for(int x : adj[i]) indeg[x]++;
+	    }
+	   queue<int> qu;
+	    for(int i=0; i<V; i++) if(indeg[i]==0) qu.push(i);
+	    while(!qu.empty())
+	    {
+	        int top=qu.front();
+	        qu.pop();
+            count++;
+	        // ans.push_back(top);
+	        for(int x: adj[top]) 
+	        {  indeg[x]--;
+	            if(indeg[x]==0) qu.push(x);
+	        }
+	    }
+        
+        return count==V;
+//         for(int i=0; i<v; i++)
+//         {
+//             if(!vis[i] and dfs(adj, vis,recst,  i)) return false;
+//         }
         return true;
     }
 };
