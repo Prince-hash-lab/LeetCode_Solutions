c03107426fb5fd134f11cf6d8c912de78da54b1d
RajRishi Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-07-06 18:54:50 +0530

    Time: 47 ms (31.36%), Space: 13.6 MB (60.34%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>& graph,vector<bool> &vis, int sr, bool &res)
-    {
-        vis[sr]=true;
-        for(int i=0; i< graph[sr].size()-1; i++)
-        {
-            int x=graph[sr][i];
-            if(!vis[x]) 
-            {
-                int y=(graph[sr].back()==-1) ? -2 :-1;
-                graph[x].push_back(y);
-                dfs(graph, vis, x, res);
-            }
-            else if(graph[x].size()>0 and graph[x].back()==graph[sr].back())
-                res=false;
+//     void dfs(vector<vector<int>>& graph,vector<bool> &vis, int sr, bool &res)
+//     {
+//         vis[sr]=true;
+//         for(int i=0; i< graph[sr].size()-1; i++)
+//         {
+//             int x=graph[sr][i];
+//             if(!vis[x]) 
+//             {
+//                 int y=(graph[sr].back()==-1) ? -2 :-1;
+//                 graph[x].push_back(y);
+//                 dfs(graph, vis, x, res);
+//             }
+//             else if(graph[x].size()>0 and graph[x].back()==graph[sr].back())
+//                 res=false;
             
-        }
-    }
+//         }
+//     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<bool> vis(n, false);
-        bool res=true;
+        vector<int> vis(n, 0);
+        // bool res=true;
+        queue<int> qu;
+        int sr;
         for(int i=0; i<n; i++)
             if(!vis[i])
             {
-                graph[i].push_back(-1);
-                dfs(graph, vis,i, res);
+                qu.push(i);
+                vis[i]=1;
+                while(!qu.empty())
+                {
+                    sr=qu.front();
+                    qu.pop();
+                    for(auto x: graph[sr])
+                    {
+                         if(!vis[x]) 
+                            {
+                                int y=(vis[sr]==1) ? 2 :1;
+                                vis[x]=y;
+                                qu.push(x);
+                            }
+                            else if(vis[x]==vis[sr])
+                                   return false;
+                    }
+                }
                 
             }
-        return res;
+        return true;
     }
 };
