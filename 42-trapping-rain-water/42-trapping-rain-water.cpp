884eb582dff0e53ad5bca10ab79c884291394753
RajRishi Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-07-07 19:07:05 +0530

    Time: 15 ms (89.03%), Space: 19.7 MB (83.09%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int sz=height.size();
-        vector<int> leftmax(sz);
-        vector<int> rightmax(sz);
-        
-        int water=0;
-        rightmax[sz-1]=height[sz-1];
-        leftmax[0]=height[0];
-        for(int i=sz-2; i>=0; i--)
+    int trap(vector<int>& arr) {
+        int low =1, high=arr.size()-2, lhigh=arr[0], rhigh=arr[high+1];
+        int wtr=0;
+        while(low<=high)
         {
-            rightmax[i]=max(height[i], rightmax[i+1]);
+            lhigh=max(lhigh, arr[low]);
+            rhigh=max(rhigh, arr[high]);
+            if(lhigh>=rhigh)
+            {
+                wtr+=rhigh-arr[high];
+                high--;
+            }
+            else if(rhigh>lhigh)
+            {
+                    wtr+=lhigh-arr[low];
+                    low++;
+            }
         }
-        for(int i=1; i<sz-1; i++)
-        {
-            leftmax[i]=max(height[i], leftmax[i-1]);
-        }
-        int val=0;
-        for(int i=1; i<sz-1; i++)
-        {
-            val=min(leftmax[i], rightmax[i]);
-            if(val<height[i]) continue;
-            else water+=(val-height[i]);
-        }
-        return water;
+        return wtr;
     }
 };
