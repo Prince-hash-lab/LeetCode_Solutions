621c2e56a1d994558b2766fb39565ebdea2941d6
RajRishi Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-07-11 18:40:44 +0530

    Time: 5 ms (50.32%), Space: 7.7 MB (55.11%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    void fperm(vector<int>& nums, int n,vector<vector<int>> &ans, unordered_map<int,int > &mp, vector<int>& v1)
+    void fperm(vector<int>& nums, int i, int n,vector<vector<int>> &ans )
     {
-        if(v1.size()==n) 
+        if(i==n-1){ans.push_back(nums); return;} 
+        for(int j=i; j<n; j++)
         {
-            ans.push_back(v1);
-            return ;
-        }
-        // for(auto x: v1) cout<<x<<" ";
-        // cout<<endl;
-        for(int j=0; j<n; j++)
-        {
-            if(!mp.count(nums[j]))
-            {
-                // cout<<" inside "<<endl;
-                mp[nums[j]]=1;
-                v1.push_back(nums[j]);
-                fperm(nums, n, ans, mp, v1);
-                v1.pop_back();
-                mp.erase(nums[j]);
-            }
+
+            swap(nums[j], nums[i]);
+            fperm(nums, i+1,n, ans );
+            swap(nums[j], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        unordered_map<int,int > mp;
-        vector<int> v1;
         int n=nums.size();
-        fperm(nums, n, ans, mp, v1);
-        // sort(ans.begin(), ans.end());
+        fperm(nums, 0, n, ans);
         return ans;
     }
 };
