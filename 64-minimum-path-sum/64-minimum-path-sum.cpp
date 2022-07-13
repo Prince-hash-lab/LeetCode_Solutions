f3b29470a739fa5e05019ddb71de836bed5fec0a
RajRishi Prince <76578997+Prince-hash-lab@users.noreply.github.com>
2022-07-13 22:19:03 +0530

    Time: 12 ms (71.22%), Space: 10.1 MB (52.10%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& cost) {
+        int n=cost.size(), m=cost[0].size();
+        vector<vector<int>> dp(n,vector<int> (m, 0));
+        dp[0][0]=cost[0][0];
+        for(int i=1; i<n; i++)
+        {
+            dp[i][0]=(dp[i-1][0]+cost[i][0]);
+        }
+        for(int i=1; i<m; i++)
+        {
+            dp[0][i]=(dp[0][i-1]+cost[0][i]);
+        }
+        for(int i=1; i<n; i++)
+        {
+            for(int j=1; j<m; j++)
+            {
+                dp[i][j]=cost[i][j]+min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
